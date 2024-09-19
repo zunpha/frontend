977447ad68acbda89a96c082c6603e6ea5eb30bd
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-09-20 08:49:03 +0900

    style: 디자인 시스템 color palette 구축

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -15,7 +15,7 @@ export function ThemedText({
   type = 'default',
   ...rest
 }: ThemedTextProps) {
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'system.negative1');
 
   return (
     <Text
