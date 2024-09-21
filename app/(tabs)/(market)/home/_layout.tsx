eec3cc07ca97195129fcb65c2f4daed9dd8e0d81
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-09-21 21:10:03 +0900

    feat: Home 페이지 NavBar selected 기준을 단순 비교에서 includes로 변경

--- app/(tabs)/(market)/home/_layout.tsx
+++ app/(tabs)/(market)/home/_layout.tsx
@@ -1,18 +1,21 @@
 import {Slot, useSegments} from "expo-router";
 import {TabBar} from "@/components/TabBar";
 import {TabItem} from "@/components/TabItem";
+import {SafeAreaView} from "react-native";
 
 export default function LayoutMarketHome() {
 	const segments: string[] = useSegments();
 
 	return (
 		<>
-			<TabBar>
-		        <TabItem path="/(market)/home/post" isActive={segments.includes('post')} label="게시물" />
-		        <TabItem path="/(market)/home/price" isActive={segments.includes('price')} label="시세 확인" />
-		        <TabItem path="/(market)/home/ranking" isActive={segments.includes('ranking')} label="랭킹" />
-			</TabBar>
-			<Slot />
+			<SafeAreaView>
+				<TabBar>
+			        <TabItem path="/(market)/home/post" isActive={segments.includes('post')} label="게시물" />
+			        <TabItem path="/(market)/home/price" isActive={segments.includes('price')} label="시세 확인" />
+			        <TabItem path="/(market)/home/ranking" isActive={segments.includes('ranking')} label="랭킹" />
+				</TabBar>
+				<Slot />
+			</SafeAreaView>
 		</>
 	)
 }
