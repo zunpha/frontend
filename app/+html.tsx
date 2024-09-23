ab65331831c0d4dfd88193f20c265a4487b9a2fa
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-09-23 09:03:19 +0900

    fix: +html.tsx 참조된 UMD 전역 변수 경고 수정

--- app/+html.tsx
+++ app/+html.tsx
@@ -1,10 +1,11 @@
 import {ScrollViewStyleReset} from 'expo-router/html';
+import {ReactNode} from "react";
 
 // This file is web-only and used to configure the root HTML for every
 // web page during static rendering.
 // The contents of this function only run in Node.js environments and
 // do not have access to the DOM or browser APIs.
-export default function Root({children}: { children: React.ReactNode }) {
+export default function Root({children}: { children: ReactNode }) {
 	return (
 		<html lang="en">
 		<head>
