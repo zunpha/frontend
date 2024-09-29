cf7357aca05d207598c50d8eb39184ed46454041
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-09-29 16:33:03 +0900

    feat: 경매 페이지에 Select 추가

--- app/(tabs)/(market)/auction.tsx
+++ app/(tabs)/(market)/auction.tsx
@@ -1,10 +1,15 @@
 import {Article} from "@/components/ui/organisms";
 import useRefreshControl from "@/hooks/useRefreshControl";
-import {RefreshControl, ScrollView} from "react-native";
-import {StyledText, View} from "@/components/ui/atoms";
+import {RefreshControl, ScrollView, StyleSheet, useColorScheme} from "react-native";
+import {Select, StyledText, View} from "@/components/ui/atoms";
 import {TextSize} from "@/enums/TextSize";
+import {useState} from "react";
+import {Colors} from "@/constants/Colors";
 
 export default function PageMarketAuction() {
+	const colorScheme = useColorScheme() ?? 'light';
+	const [filteredCategory, setFilteredCategory] = useState<string>('');
+
 	const img_url = 'https://image.ajd.co.kr/EDITOR/CONTENT/e5723007-c027-4cdb-bf5b-6e260b0e1796';
 
 	const {refreshControlProps} = useRefreshControl({
@@ -15,21 +20,48 @@ export default function PageMarketAuction() {
 	});
 
 	return (
-		<ScrollView refreshControl={
-			<RefreshControl {...refreshControlProps} />
-		}>
-			<Article imageUrl={img_url} title={'ㅇ️'}>
-				<StyledText size={TextSize.LabelLarge} color={'grayScale.primary70'}>입찰 시작가</StyledText>
-				<Article.PriceText price={500_000}/>
-				<View style={{
-					display: 'flex',
-					flexDirection: 'row',
-					gap: 8
-				}}>
-					<Article.AuctionParticipatingUser />
-					<Article.AuctionRemainingTime />
-				</View>
-			</Article>
-		</ScrollView>
+		<>
+			<View style={{
+				...styles.filterContainer,
+				borderColor: Colors[colorScheme]['grayScale.primary20'],
+			}}>
+				<Select
+					name={'가격'}
+					state={filteredCategory}
+					setState={setFilteredCategory}
+					render={
+						<StyledText size={TextSize.BodyLarge} color={'grayScale.primary70'}>가격</StyledText>
+					}
+				/>
+			</View>
+			<ScrollView refreshControl={
+				<RefreshControl {...refreshControlProps} />
+			}>
+				<Article imageUrl={img_url} title={'ㅇ️'}>
+					<StyledText size={TextSize.LabelLarge} color={'grayScale.primary70'}>입찰 시작가</StyledText>
+					<Article.PriceText price={500_000}/>
+					<View style={{
+						display: 'flex',
+						flexDirection: 'row',
+						gap: 8
+					}}>
+						<Article.AuctionParticipatingUser/>
+						<Article.AuctionRemainingTime/>
+					</View>
+				</Article>
+			</ScrollView>
+		</>
 	)
 }
+
+const styles = StyleSheet.create({
+	filterContainer: {
+		display: 'flex',
+		flexDirection: 'row',
+
+		paddingHorizontal: 16,
+		paddingVertical: 8,
+
+		borderBottomWidth: 1,
+	}
+})
