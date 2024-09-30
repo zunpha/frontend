74ba6e6105cdc5e5392d276d2472d0477dc07049
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-09-30 19:28:12 +0900

    feat: 모달을 전역으로 관리하기 위한 useModal.ts
    zustand에 Modal visible state를 객체로 저장해 쉽게 import해 사용할 수 있게 제공
    
    사용방법
    const { openModal, closeModal, isModalOpen } = useModal();

---
+++ stores/Modal.ts
@@ -0,0 +1,19 @@
+// 모달 상태를 관리하는 Zustand store 생성
+import {create} from "zustand";
+
+interface ModalState {
+	modals: { [id: string]: boolean };  // 모달 ID에 따른 상태
+	openModal: (id: string) => void;    // 특정 모달 열기
+	closeModal: (id: string) => void;   // 특정 모달 닫기
+	isModalOpen: (id: string) => boolean;  // 특정 모달이 열려 있는지 확인
+}
+
+const useModal = create<ModalState>((set, get) => ({
+	modals: {},
+	openModal: (id) => set((state) => ({modals: {...state.modals, [id]: true}})),
+	closeModal: (id) => set((state) => ({modals: {...state.modals, [id]: false}})),
+	isModalOpen: (id: string) => !!get().modals[id],
+}));
+
+
+// const { openModal, closeModal, isModalOpen } = useModal();
