1e5493f86fdcab334d368859b3bcf260221e76af
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-10-05 18:54:42 +0900

    feat: 프로필 편집하기 메뉴 추가

--- app/(tabs)/(market)/profile.tsx
+++ app/(tabs)/(market)/profile.tsx
@@ -5,6 +5,8 @@ import {useNavigation} from "expo-router";
 import {StyleSheet, useColorScheme} from "react-native";
 import {Colors} from "@/constants/Colors";
 import ZPayIcon from "@/components/icons/ZPay";
+import SampleProfile from '@/assets/images/sample_profile.png';
+import {Image} from 'expo-image';
 
 export default function PageProfile() {
 	const navigation = useNavigation();
@@ -17,34 +19,67 @@ export default function PageProfile() {
 		}}>
 			<View style={styles.section}>
 				<ProfileSectionHref
-					leftRender={<StyledText size={TextSize.HeadingSmall} color={'grayScale.primary100'}>팔로우</StyledText>}
-					onPress={() => {}}
+					leftRender={
+						<View style={{
+							flexDirection: 'row',
+							alignItems: 'center',
+							paddingTop: 6,
+						}}>
+							<Image source={SampleProfile} style={{
+								...styles.profile,
+								borderColor: Colors[colorScheme]['grayScale.primary40'],
+							}}/>
+							<View style={{
+								gap: 4
+							}}>
+								<StyledText size={TextSize.BodyLarge} color={'grayScale.primary90'}>서울거래킹</StyledText>
+								<StyledText size={TextSize.LabelLarge} color={'grayScale.primary60'}>프로필
+									편집하기</StyledText>
+							</View>
+						</View>
+					}
+					onPress={() => {
+					}}
 				/>
 				<ProfileSectionHref
-					leftRender={<StyledText size={TextSize.HeadingSmall} color={'grayScale.primary100'}>내 상품</StyledText>}
-					onPress={() => {}}
+					leftRender={<StyledText size={TextSize.HeadingSmall}
+					                        color={'grayScale.primary100'}>팔로우</StyledText>}
+					onPress={() => {
+					}}
+				/>
+				<ProfileSectionHref
+					leftRender={<StyledText size={TextSize.HeadingSmall} color={'grayScale.primary100'}>내
+						상품</StyledText>}
+					onPress={() => {
+					}}
 				/>
 			</View>
 			<View style={{
 				height: 20,
 				backgroundColor: Colors[colorScheme]['grayScale.primary10'],
-			}} />
+			}}/>
 			<View style={styles.section}>
 				<ProfileSectionHref
 					leftRender={<ZPayIcon/>}
-					onPress={() => {}}
+					onPress={() => {
+					}}
 				/>
 				<ProfileSectionHref
 					leftRender={<StyledText size={TextSize.HeadingSmall} color={'grayScale.primary100'}>알림</StyledText>}
-					onPress={() => {}}
+					onPress={() => {
+					}}
 				/>
 				<ProfileSectionHref
-					leftRender={<StyledText size={TextSize.HeadingSmall} color={'grayScale.primary100'}>거래 기록</StyledText>}
-					onPress={() => {}}
+					leftRender={<StyledText size={TextSize.HeadingSmall} color={'grayScale.primary100'}>거래
+						기록</StyledText>}
+					onPress={() => {
+					}}
 				/>
 				<ProfileSectionHref
-					leftRender={<StyledText size={TextSize.HeadingSmall} color={'grayScale.primary100'}>관심 목록</StyledText>}
-					onPress={() => {}}
+					leftRender={<StyledText size={TextSize.HeadingSmall} color={'grayScale.primary100'}>관심
+						목록</StyledText>}
+					onPress={() => {
+					}}
 				/>
 			</View>
 		</View>
@@ -59,7 +94,13 @@ const styles = StyleSheet.create({
 		paddingVertical: 12,
 		gap: 12,
 	},
-	divider: {
+	profile: {
+		width: 40,
+		height: 40,
 
+		marginRight: 12,
+
+		borderRadius: 20,
+		borderWidth: 1,
 	}
 })
